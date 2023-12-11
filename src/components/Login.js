a5378e98196570653f34777a16441718dfb46e8e
kushal <mohitsinghkpsc@gmail.com>
2023-12-12 00:18:56 +0530

    Firebase configuration done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,31 @@
 import Header from "./Header"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { checkValidate } from "../utils/validate";
 const Login = () => {
 
   const [isSignInForm,setIsSignInForm] = useState(true);
+  const [errorMessage,setErrorMessage] = useState("");
+
+  const email = useRef(null);
+  const password = useRef(null);
   const toggleSignForm = ()=>{
         setIsSignInForm(!isSignInForm);
+  }
+
+  const handleClick = ()=>{
+     console.log(email.current.value);
+     console.log(password.current.value);
+
+     const message = checkValidate(email.current.value,password.current.value);
+     setErrorMessage(message);
   }
   return (
     <div>
       <Header/>
       <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="bg-image" />
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="bgimage" />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 m-auto right-0 left-0 text-white bg-opacity-80 rounded-lg">
+      <form onSubmit={(e)=>e.preventDefault()}  className="w-3/12 absolute p-12 bg-black my-36 m-auto right-0 left-0 text-white bg-opacity-80 rounded-lg">
         <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm &&  <input
          type="text" 
@@ -20,9 +33,10 @@ const Login = () => {
          className="p-4 my-4 w-full bg-gray-700 rounded-bl-md"
          /> }
        
-        <input type="text" placeholder="Enter email / Mob no." className="p-4 my-4 w-full bg-gray-700 rounded-md" />
-        <input type="Password" placeholder="Enter your password." className="p-4 my-2  w-full bg-gray-700 rounded-md" />
-        <button className="p-4 my-6 w-full text-white bg-red-700 rounded-lg">Sign In</button>
+        <input ref={email} type="text" placeholder="Enter email / Mob no." className="p-4 my-4 w-full bg-gray-700 rounded-md" />
+        <input ref={password} type="Password" placeholder="Enter your password." className="p-4 my-2  w-full bg-gray-700 rounded-md" />
+        <p className={`font-extrabold ${(errorMessage === null && errorMessage !== "") ? "text-green-700" : "text-red-700"  }`}>{(errorMessage === null && errorMessage !== "") ? "Successful!" : errorMessage}</p>
+        <button className="p-4 my-6 w-full text-white bg-red-700 rounded-lg" onClick={handleClick}>Sign In</button>
 
         <div className="inline-flex">
             <p className="text-gray-400 mx-1">{isSignInForm ? "New to Netflix? " : "Already Registered"}</p>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAdhS3MQkMZzwV-9zYWixN-qdGHBwB5qfM",
+  authDomain: "netflixgpt-6a6bc.firebaseapp.com",
+  projectId: "netflixgpt-6a6bc",
+  storageBucket: "netflixgpt-6a6bc.appspot.com",
+  messagingSenderId: "115011963997",
+  appId: "1:115011963997:web:0a10827cefc6e8538d11b4",
+  measurementId: "G-GQRWBZ95DE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
