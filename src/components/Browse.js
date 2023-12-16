2a1237b73e8a722f8c972c5d5663dd994eba34fb
kushal <mohitsinghkpsc@gmail.com>
2023-12-16 22:22:01 +0530

    register tmdb api & create an app & get data from tmdb done

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react"
+import { OPTIONS_API } from "../utils/constants"
 import Header from "./Header"
 
 const Browse = () => {
+
+  useEffect(()=>{
+    getNowPlayingMovie();
+  },[]);
+
+
+  const getNowPlayingMovie = async()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',OPTIONS_API);
+    const json = await data.json();
+    console.log(json)
+
+  }
   return (
     <div>
       <Header/>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const PROFILE_URL = "https://avatars.githubusercontent.com/u/130832724?v=4";
+
+
+export const OPTIONS_API = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTgxMjdjMTA5YjQyZjI0ZjIxOGJhNWNmYjM5YjVjMiIsInN1YiI6IjY1N2RkMDE4N2FkMDhjMDY5NzUwMDY2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8nf3Zfh2eKkrQA9Ai0N1azYyOGSwJ1gPxsUxQJR5cGE'
+    }
+  };
